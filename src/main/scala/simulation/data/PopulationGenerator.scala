e18db1f55fa2bdee3ffbf01f2a4a9afd2a94407b
Anas Boujatouy <anas@air-de-anas-2.home>
2024-04-24 20:28:00 +0200

    ajout de la fonction de génération de data

--- src/main/scala/simulation/data/PopulationGenerator.scala
+++ src/main/scala/simulation/data/PopulationGenerator.scala
@@ -1,1 +1,18 @@
-case class SensorData(sensorId: String, timestamp: Long, weight: Double)
+// SensorSimulator.scala
+import scala.util.Random
+
+object SensorSimulator extends App {
+  val random = new Random()
+
+  def generateWeight(): Double = 5 + random.nextDouble() * (500 - 5)
+
+  def simulateSensorData(sensorId: String): SensorData = {
+    val weight = generateWeight()
+    SensorData(sensorId, System.currentTimeMillis(), weight)
+  }
+
+  val sensorId = "sensor-001"
+  val simulatedData = simulateSensorData(sensorId)
+
+  println(s"Simulated Data: $simulatedData")
+}

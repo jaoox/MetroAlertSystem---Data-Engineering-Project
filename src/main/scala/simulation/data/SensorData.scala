96e28de3a8468fbb41380e7ffef266ec99d54267
AngeleBlais <94390537+AngeleBlais@users.noreply.github.com>
2024-04-25 16:06:55 +0200

    test

--- src/main/scala/simulation/data/SensorData.scala
+++ src/main/scala/simulation/data/SensorData.scala
@@ -5,17 +5,49 @@ import scala.util.Random
 case class SensorData(sensorId: String, timestamp: Long, weight: Double)
 
 object SensorSimulator extends App {
-  val random = new Random()
+  private val random = new Random()
+  private val minWeight = 10.0 
+  private val maxWeight = 1000.0 
 
-  def generateWeight(): Double = 5 + random.nextDouble() * (500 - 5)
+  def generateWeight(): Double = {
+    minWeight + random.nextDouble() * (maxWeight - minWeight)
+  }
 
   def simulateSensorData(sensorId: String): SensorData = {
     val weight = generateWeight()
     SensorData(sensorId, System.currentTimeMillis(), weight)
   }
 
-  val sensorId = "sensor-001"
-  val simulatedData = simulateSensorData(sensorId)
+  def simulateMultipleSensorData(sensorIds: Seq[String]): Seq[SensorData] = {
+    sensorIds.map(sensorId => simulateSensorData(sensorId))
+  }
+}
 
-  println(s"Simulated Data: $simulatedData")
+case class Person(id: Int, position: Double, speed: Double)
+
+object PopulationGenerator {
+  private val random = new Random()
+
+  def generateInitialPopulation(numPeople: Int, railLength: Double): List[Person] = {
+=======
+object PopulationGenerator {
+  def generateInitialPopulation(numPeople: Int, railLength: Double, scenario: String): List[Person] = {
+    val random = new Random()
+    val initialSpeed = scenario match {
+      case "rush_hour" => 5.0 // Vitesse élevée pendant les heures de pointe
+      case "off_peak" => 3.0 // Vitesse plus lente en période creuse
+      case _ => 4.0 // Vitesse moyenne par défaut
+    }
+>>>>>>> f550f1c73e835b3f7c4af04b30249978da3faa2c
+    (1 to numPeople).map { id =>
+      // Position aléatoire le long des rails de métro
+      val position = random.nextDouble() * railLength
+<<<<<<< HEAD
+      val speed = random.nextDouble() * 5
+      Person(id, position, speed)
+=======
+      new Person(id, position, initialSpeed)
+>>>>>>> f550f1c73e835b3f7c4af04b30249978da3faa2c
+    }.toList
+  }
 }

fffb8f13ac4a6c9b6aa9db56ef647929255ce844
Anas Boujatouy <anas@air-de-anas-2.home>
2024-05-19 17:33:28 +0200

    modif kafka

--- src/main/scala/KafkaDataProducer.scala
+++ src/main/scala/KafkaDataProducer.scala
@@ -1,4 +1,5 @@
 import java.util.Properties
+import scala.io.Source
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
 object KafkaDataProducer {
@@ -7,17 +8,17 @@ object KafkaDataProducer {
     props.put("bootstrap.servers", "localhost:9092")
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
     props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-
     val producer = new KafkaProducer[String, String](props)
 
-    val data = List(
-      """{"Timestamp": 8, "Station": "Ligne 8", "PersonId": 11, "Hour": 8, "Position": 219.3934086212331, "Speed": 4.0, "Scenario": "off"}""",
-      """{"Timestamp": 8, "Station": "Ligne 8", "PersonId": 12, "Hour": 8, "Position": 804.6359635867948, "Speed": 4.0, "Scenario": "off"}""",
-      """{"Timestamp": 8, "Station": "Ligne 8", "PersonId": 13, "Hour": 8, "Position": 894.6087557599926, "Speed": 4.0, "Scenario": "off"}"""
-    )
+    // Chemin vers le fichier JSON
+    val filePath = "simulation_data.json"
+    val source = Source.fromFile(filePath)
+    val data = source.getLines().toList
+    source.close()
 
-    data.foreach { record =>
-      producer.send(new ProducerRecord[String, String]("metro-data", record))
+    // Envoi des données à Kafka
+    data.foreach { jsonLine =>
+      producer.send(new ProducerRecord[String, String]("metro-data", jsonLine))
     }
 
     producer.close()

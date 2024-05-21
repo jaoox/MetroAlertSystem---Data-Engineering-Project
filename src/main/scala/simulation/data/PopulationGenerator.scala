d1ddf4fbb83e2084b7cbc362f12e361dad511fc2
AngeleBlais <94390537+AngeleBlais@users.noreply.github.com>
2024-05-21 20:14:34 +0200

    kafka producer

--- src/main/scala/simulation/data/PopulationGenerator.scala
+++ src/main/scala/simulation/data/PopulationGenerator.scala
@@ -3,18 +3,15 @@ package simulation.data
 import scala.util.Random
 import simulation.model._
 
-import scala.util.Random
-
 case class Person(id: Int, position: Double, speed: Double)
 
 object PopulationGenerator {
   private val random = new Random()
 
-  // Définition des vitesses correspondant à différents scénarios
   object Speeds {
-    val RushHour = 5.0 // Vitesse élevée pendant les heures de pointe
-    val OffPeak = 3.0 // Vitesse plus lente en période creuse
-    val Default = 4.0 // Vitesse moyenne par défaut
+    val RushHour = 5.0 
+    val OffPeak = 3.0 
+    val Default = 4.0 
   }
 
   def generateInitialPopulation(numPeople: Int, railLength: Double, scenario: String): List[Person] = {
